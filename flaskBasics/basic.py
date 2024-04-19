3e56d6d5ef3a1147c63e0fa605ee7bad0f5b62de
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-04-19 17:48:14 +0530

    working with basic and dynamic routing

--- flaskBasics/basic.py
+++ flaskBasics/basic.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 def index():
     return "<hi>Welcome to my first web page!</h1>"
 
+#working with basic routing
+@app.route("/information")
+def info():
+    return "<hi>I am new to Flask</hi>"
+
+#dynamic routing
+@app.route("/human/<name>")
+def viewhuman(name):
+    return "<h1>This is a page for {}</h1>".format(name.upper())
+
 if __name__=="__main__":
     app.run()
