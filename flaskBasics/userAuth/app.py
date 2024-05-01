891cc3201d4305327004760496e5e48012fe3c03
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-01 19:33:06 +0530

    configuring the welcome page and logout views

--- flaskBasics/userAuth/app.py
+++ flaskBasics/userAuth/app.py
@@ -7,3 +7,15 @@ from myproject.forms import LoginForm, RegistrationForm
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/welcome')
+@login_required #user has to be logged in to see this view
+def welcome_user():
+    return render_template('welcome_user.html')    
+
+@app.route('/logout')
+@login_required #user has to be logged in to see this view
+def logout():
+    logout_user() #logout the user using flask_login imports
+    flash("You logged out!")
+    return redirect(url_for('home'))
