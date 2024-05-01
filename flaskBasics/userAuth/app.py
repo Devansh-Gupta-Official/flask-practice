2d2f4d3ab95f21604c62616fe82aa48a3be8b07d
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-05-01 19:42:23 +0530

    configuring login and register views

--- flaskBasics/userAuth/app.py
+++ flaskBasics/userAuth/app.py
@@ -19,3 +19,36 @@ def logout():
     logout_user() #logout the user using flask_login imports
     flash("You logged out!")
     return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit:
+        user=User.query.filter_by(email=form.email.data).first()
+
+        if user.check_password(form.password.data) and user is not None:
+            login_user(user)   #done automatically
+            flash('Logged in successfully')
+            next = request.args.get('next')    #basically pages that require login that the user was accessing before logging in
+
+            #check if the next request actually exists
+            if next == None or not next[0]=='/':
+                next = url_for('welcome_user')
+
+            return redirect(next)  
+        
+        return render_template('login.html', form=form)
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form =RegistrationForm()
+    if form.validate_on_submit:
+        user=User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Thanks for registering! Now you can login!')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', form=form)
