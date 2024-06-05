dfd1e21ad93d8641bf1e4a969fa9a357f82d7f7c
Devansh-Gupta-Official <03devansh.gupta@gmail.com>
2024-06-05 18:32:10 +0530

    basic crud with rest apis

--- flaskBasics/flask-rest/api.py
+++ flaskBasics/flask-rest/api.py
@@ -5,11 +5,37 @@ app=Flask(__name__)
 
 api=Api(app)
 
-class HelloWorld(Resource):
-    def get(self): #GET method
-        return jsonify({'hello':'world'})
+# class HelloWorld(Resource):  
+#     def get(self): #GET method
+#         return jsonify({'hello':'world'})
 
-api.add_resource(HelloWorld,'/')
+
+# {'name':'Rufus'},{'name':'Fido'},{'name':'Spike'},.....
+puppies=[]
+class PuppyNames(Resource):
+    def get(self,name):
+        for pup in puppies:
+            if pup['name']==name:
+                return pup
+        return {'name':None},404   #404 is the status code for not found and it is returned when the name is not found in the list of puppies
+    
+    def post(self,name):
+        pup={'name':name}
+        puppies.append(pup)
+        return pup
+
+    def delete(self,name):
+        for ind,pup in enumerate(puppies):
+            if pup['name']==name:
+                deleted_pup=puppies.pop(ind)
+                return {'note':'delete success'}
+
+class AllNames(Resource):
+    def get(self):
+        return {'puppies':puppies}
+
+api.add_resource(PuppyNames,'/puppy/<string:name>')
+api.add_resource(AllNames,'/puppies')
 
 if __name__ == '__main__':
     app.run(debug=True)
